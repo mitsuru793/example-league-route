daca012db9d9d55405fad3f5b147ae6075c1e4ab
mitsuru793 <mitsuru793@gmail.com>
2019-08-06 16:03:31 +0900

    add hello world with league/route

---
+++ src/index.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types=1);
+
+include __DIR__ . '/../vendor/autoload.php';
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+$request = Zend\Diactoros\ServerRequestFactory::fromGlobals(
+    $_SERVER, $_GET, $_POST, $_COOKIE, $_FILES
+);
+
+$router = new League\Route\Router;
+
+// map a route
+$router->map('GET', '/', function (ServerRequestInterface $request): ResponseInterface {
+    $response = new Zend\Diactoros\Response;
+    $response->getBody()->write('<h1>Hello, World!</h1>');
+    return $response;
+});
+
+$response = $router->dispatch($request);
+
+// send the response to the browser
+(new Zend\HttpHandlerRunner\Emitter\SapiEmitter)->emit($response);
